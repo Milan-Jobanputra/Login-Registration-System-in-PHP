a5c1a3a12c67a6d28427de105bf5235c21bc2255
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2024-01-10 18:16:26 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
